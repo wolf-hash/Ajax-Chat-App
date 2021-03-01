eded220987724cae7753aad9d933065435f54a0a
wolf-hash <karansingh8777@gmail.com>
2021-03-01 22:05:25 +0530

    requirements.txt

---
+++ ajax_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/media/wolf/OS/Users/Rajneesh/Web Development/Ajax Chat App/ajax_env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
